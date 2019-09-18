d045522c057034d1589c926736fe7503349fb154
Damian Bielecki <bielik20@gmail.com>
2019-09-18 09:15:46 +0200

    add bind to class spec

---
+++ e2e/bind-to-class.spec.ts
@@ -0,0 +1,68 @@
+import { Container, Injectable } from '../src';
+
+@Injectable()
+class DependencyClass {
+  name = 'original dependency';
+}
+
+@Injectable()
+class MasterClass {
+  name = 'original master';
+
+  constructor(public dep: DependencyClass) {}
+}
+
+@Injectable()
+class DependencyClassImpl implements DependencyClass {
+  name = 'implementation dependency';
+}
+
+@Injectable()
+class MasterClassImpl implements MasterClass {
+  name = 'implementation master';
+
+  constructor(public dep: DependencyClass) {}
+}
+
+describe('bind to class', () => {
+  let container: Container;
+
+  beforeEach(() => {
+    container = new Container();
+  });
+
+  it('should work for original classes', () => {
+    const instance = container.get(MasterClass);
+
+    expect(instance.name).toBe('original master');
+    expect(instance.dep.name).toBe('original dependency');
+  });
+
+  it('should work for implementation classes', () => {
+    container.bind(MasterClass).to(MasterClassImpl);
+    container.bind(DependencyClass).to(DependencyClassImpl);
+
+    const instance = container.get(MasterClass);
+
+    expect(instance.name).toBe('implementation master');
+    expect(instance.dep.name).toBe('implementation dependency');
+  });
+
+  it('should work for original master and implementation dependency', () => {
+    container.bind(DependencyClass).to(DependencyClassImpl);
+
+    const instance = container.get(MasterClass);
+
+    expect(instance.name).toBe('original master');
+    expect(instance.dep.name).toBe('implementation dependency');
+  });
+
+  it('should work for implementation master and original dependency', () => {
+    container.bind(MasterClass).to(MasterClassImpl);
+
+    const instance = container.get(MasterClass);
+
+    expect(instance.name).toBe('implementation master');
+    expect(instance.dep.name).toBe('original dependency');
+  });
+});
